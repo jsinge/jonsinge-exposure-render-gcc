3cecaf654813c5916b8249e58013eb88d0efa297
Jonas Singe <jonsinge@freenet.de>
2012-06-25 01:14:47 +0200

    additional #ifdef to avoid dll declspec in non-windows-systems

--- Source/Dll.h
+++ Source/Dll.h
@@ -13,8 +13,12 @@
 
 #pragma once
 
-#ifdef _EXPORTING
-	#define EXPOSURE_RENDER_DLL    __declspec(dllexport)
+#ifdef WIN32
+    #ifdef _EXPORTING
+        #define EXPOSURE_RENDER_DLL    __declspec(dllexport)
+    #else
+        #define EXPOSURE_RENDER_DLL    __declspec(dllimport)
+    #endif
 #else
-	#define EXPOSURE_RENDER_DLL    __declspec(dllimport)
+    #define EXPOSURE_RENDER_DLL
 #endif
